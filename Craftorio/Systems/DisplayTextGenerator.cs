0d88bbf19f6d3e6e3a0b8092f22573a7c1e71142
Edgar Carballo <karvayoEdgar@gmail.com>
2022-07-16 17:54:31 -0700

    Fix some bug in the tooltip generator system

--- Craftorio/Systems/DisplayTextGenerator.cs
+++ Craftorio/Systems/DisplayTextGenerator.cs
@@ -3,14 +3,35 @@ using System;
 using DefaultEcs.System;
 using Microsoft.Xna.Framework.Graphics;
 
+/// <summary>
+/// Produce tooltip entities from mouseover events.
+/// </summary>
 public sealed class DisplayTextGenerator : AEntitySetSystem<int>
 {
+    /// <summary>
+    /// Time until the tooltip is shown.
+    /// </summary>
     private const int MouseOverDisplayTextTime = 1000; // 1 second.
-
+    /// <summary>
+    /// Collection of tooltip entities.
+    /// </summary>
     private readonly EntitySet toolTipEntities;
+
+    /// <summary>
+    /// When this field is set to true, all (at most only one could exist.) tooltip entities will be removed.
+    /// </summary>
     private bool IsClearingInfoBoxes = false;
+
     private OrthographicCamera? camera;
+
+    /// <summary>
+    /// Measure the time the mouse have been over an entity.
+    /// </summary>
     private int mouseOverTimer;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DisplayTextGenerator"/> class.
+    /// </summary>
     public DisplayTextGenerator(World world) :
         base(world.GetEntities()
             .With<MouseOverDisplayText>()
@@ -21,8 +42,16 @@ public sealed class DisplayTextGenerator : AEntitySetSystem<int>
             .With<IsInfoBox>()
             .AsSet();
     }
-    public SpriteFont Font { get; set; }
 
+    /// <summary>
+    /// Font used to produce the tooltip entities.
+    /// </summary>
+    /// <value>A value must be set, otherwise an exception could be thrown.</value>
+    public SpriteFont Font { get; init; }
+
+    /// <summary>
+    /// After the update call, if the <see cref="IsClearingInfoBoxes"/> field is set to true, all tooltip entities will be removed.
+    /// </summary>
     protected override void PostUpdate(int state)
     {
         if (IsClearingInfoBoxes && toolTipEntities.Count > 0)
@@ -32,6 +61,10 @@ public sealed class DisplayTextGenerator : AEntitySetSystem<int>
         }
     }
 
+    /// <summary>
+    /// Updates the camera.
+    /// </summary>
+    /// <param name="state"></param>
     protected override void PreUpdate(int state)
     {
         // Get the camera from the world
@@ -40,11 +73,8 @@ public sealed class DisplayTextGenerator : AEntitySetSystem<int>
         IsClearingInfoBoxes = false;
     }
 
-    protected override void Update(int state, ReadOnlySpan<Entity> entities)
+    protected void Update(int state, in Entity entity, out bool entityCatched)
     {
-        // Only update the first entity.
-        var entity = entities[0];
-
         // Get the location and display state of this entity
         ref var location = ref entity.Get<Location>();
         ref var displayState = ref entity.Get<MouseOverDisplayText>();
@@ -52,7 +82,7 @@ public sealed class DisplayTextGenerator : AEntitySetSystem<int>
         // if the mouse is over this entity:
         var mouseScreenLocation = Microsoft.Xna.Framework.Input.Mouse.GetState().Position.ToVector2();
         var mouseWorldLocation = camera!.ScreenToWorld(mouseScreenLocation);
-        displayState.IsCurrentlyHovered = location.Bounds.Contains(mouseWorldLocation);
+        entityCatched = displayState.IsCurrentlyHovered = location.Bounds.Contains(mouseWorldLocation);
         if (displayState.IsCurrentlyHovered)
         {
             // If the mouse is not currently displayed, update the timer.
@@ -88,6 +118,18 @@ public sealed class DisplayTextGenerator : AEntitySetSystem<int>
                 IsClearingInfoBoxes = true;
                 displayState.IsCurrentlyDisplayed = false;
             }
+        }
+    }
+    protected override void Update(int state, ReadOnlySpan<Entity> entities)
+    {
+        // Only update the first entity.
+        foreach (ref readonly var entity in entities)
+        {
+            Update(state, entity, out var entityCatched);
+
+            // If an entity got the mouse over state, we can stop the loop.
+            if (entityCatched)
+                break;
         }
     }
 }
